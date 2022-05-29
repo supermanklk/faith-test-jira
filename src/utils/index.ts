d52dc1237823f1196552957a4d917089100b1b11
bin.zhang02 <bin.zhang02@weimob.com>
2022-05-29 20:55:08 +0800

    feat: 忽略ts的报错 // @ts-ignore

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,9 +1,12 @@
-export const isFalsy = (value) => (value === 0 ? false : !value)
-export const cleanObject = (object) => {
+export const isFalsy = (value: unknown): boolean => (value === 0 ? false : !value)
+export const cleanObject = (object: object) => {
   const result = { ...object }
   Object.keys(result).forEach((key) => {
+    // @ts-ignore
+    // 解决这一块的只是需要用到泛型
     const value = result[key]
     if (isFalsy(value)) {
+      // @ts-ignore
       delete result[key]
     }
   })
