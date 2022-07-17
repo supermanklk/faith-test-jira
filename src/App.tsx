49a8dcee5319c410c8040cec5592b306bf10a359
supermanklk <739367755@qq.com>
2022-07-17 22:24:11 +0800

    feat: 完成主页 css in js 多场景使用

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,9 @@ import styled from '@emotion/styled'
 import { useAuth } from './context/authContext'
 import AuthenticatedApp from './authenticatedApp'
 import UnauthenticatedApp from './unauthenticatedApp'
+import panda from './asset/images/panda.svg'
+import left from './asset/images/left.svg'
+import right from './asset/images/right.svg'
 import 'antd/dist/antd.css'
 
 // 获取环境变量的地址
@@ -19,11 +22,13 @@ function App() {
 
   // 用css in js 代替 之前的内联写法
   return (
-    <Container style={{ display: 'flex', justifyContent: 'center' }}>
+    <Container>
+      <Header />
+      <Background />
       {/* 此刻如果我们改写Card的样式 传统的方式就是去找这个组件给的属性比如 className 、bodyStyle 此刻我们可以直接通过 emotion来做 */}
       {/* <Card>切换到{user ? '登录' : '注册'}</Card> */}
       <CardWrapper>
-        {' '}
+        <Title>请登录</Title>
         {user ? <AuthenticatedApp /> : <UnauthenticatedApp />}
         <Divider />
         切换到{user ? '登录' : '注册'}
@@ -32,9 +37,32 @@ function App() {
   )
 }
 
+const Title = styled.h2``
+
+const Background = styled.div`
+  position: absolute;
+  width: 100%;
+  height: 100%;
+  background-repeat: no-repeat;
+  background-attachment: fixed;
+  background-position: left bottom, right bottom;
+  background-image: url(${left}), url(${right});
+  background-size: calc(((100vw - 40rem) / 2) - 3.2rem), calc(((100vw - 40rem) / 2) - 3.2rem), cover;
+`
+
+const Header = styled.header`
+  width: 100%;
+  background: url(${panda}) no-repeat center;
+  padding: 5rem 0;
+  background-size: 8rem;
+`
+
 const Container = styled.div`
   display: flex;
-  justify-content: center;
+  /* justify-content: center; */
+  flex-direction: column;
+  align-items: center;
+  min-height: 100vh;
 `
 
 const CardWrapper = styled(Card)`
