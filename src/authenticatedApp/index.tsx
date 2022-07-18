8f9a0545db8ad76ea913ebcb1113addd311b342a
supermanklk <739367755@qq.com>
2022-07-18 23:09:50 +0800

    feat: 使用Grid布局完成双飞翼布局
    效果图如下：
    http://faith-2021-001.oss-cn-hangzhou.aliyuncs.com/20220718/165815693797148.png

--- src/authenticatedApp/index.tsx
+++ src/authenticatedApp/index.tsx
@@ -1,3 +1,4 @@
+import styled from '@emotion/styled'
 import React from 'react'
 import { useAuth } from '../context/authContext'
 import ProjectList from '../pages/ProjectList'
@@ -5,18 +6,70 @@ import TryUseArray from '../pages/TryUseArray'
 const AuthenticatedApp = () => {
   const { logout } = useAuth()
   return (
-    <div>
-      AuthenticatedApp
-      <ProjectList />
-      <TryUseArray />
-      <button
-        onClick={() => {
-          logout()
-        }}>
-        退出
-      </button>
-    </div>
+    <Container>
+      <Header>
+        <HeaderLeft>
+          <div>logo</div> <div>项目</div> <div>用户</div>{' '}
+        </HeaderLeft>
+        <HeaderRight>
+          <button
+            onClick={() => {
+              logout()
+            }}>
+            退出
+          </button>
+        </HeaderRight>
+      </Header>
+      <Nav>left</Nav>
+      <Main>
+        <ProjectList />
+        <TryUseArray />
+      </Main>
+      <Aside>right</Aside>
+      <Footer>footer</Footer>
+    </Container>
   )
 }
+
+const Container = styled.div`
+  display: grid;
+  grid-template-rows: 6rem 1fr 6rem;
+  grid-template-columns: 20rem 1fr 20rem;
+  grid-template-areas: 'header header header' 'nav main aside' 'footer footer footer';
+  height: 100vh;
+`
+
+const Header = styled.header`
+  background: gray;
+  grid-area: header;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+`
+
+const HeaderLeft = styled.div`
+  display: flex;
+  align-items: center;
+`
+const HeaderRight = styled.div``
+
+const Nav = styled.nav`
+  background-color: yellow;
+  grid-area: nav;
+`
+
+const Main = styled.main`
+  grid-area: main;
+`
+
+const Aside = styled.aside`
+  background-color: yellow;
+  grid-area: aside;
+`
+
+const Footer = styled.footer`
+  background-color: tomato;
+  grid-area: footer;
+`
 
 export default AuthenticatedApp
